6220d2e8391d6a94b4f1c2891fac3541db57347a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-06 13:52:34 +0530

    added card and added styling on card and container

--- components/ElevatedCards.tsx
+++ components/ElevatedCards.tsx
@@ -5,9 +5,9 @@ const ElevatedCards = (): JSX.Element => {
   return (
     <View>
       <Text style={styles.headingText}>Elevated Cards</Text>
-      <ScrollView>
-        <View>
-          <Text>TAP!</Text>
+      <ScrollView style={styles.container}>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text style={styles.cardText}>TAP!</Text>
         </View>
       </ScrollView>
     </View>
@@ -22,5 +22,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingHorizontal: 8,
     paddingVertical: 6,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 8,
+  },
+  card: {
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cardElevated: {
+    backgroundColor:"#dadada"
+  },
+  cardText: {
+    color: '#000',
   },
 });
