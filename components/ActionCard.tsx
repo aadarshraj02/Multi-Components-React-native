773ca6fe5432f6b9d2d92708e9d15b88df0c50be
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-06 17:38:09 +0530

    added function to open url using Linking

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -1,7 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Linking, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const ActionCard = (): JSX.Element => {
+  const openWebsite = (websiteLink: string) => {
+    Linking.openURL(websiteLink);
+  };
+
   return (
     <View>
       <Text>ActionCard</Text>
