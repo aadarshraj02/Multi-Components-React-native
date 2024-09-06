85d9405c4592e83a0396868a7b457ac851ddecad
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-06 09:10:46 +0530

    added type of FlatCards

--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-export default function FlatCards() {
+export default function FlatCards(): JSX.Element {
   return (
     <View>
       <Text>FlatCards</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
