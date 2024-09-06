26002d9a8e41034bfb2a8d29e98688a0f5433b71
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-06 23:41:25 +0530

    added contact List

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -1,8 +1,15 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const ContactList = () => {
-  const contacts = [
+type Contact = {
+  uid: number;
+  name: string;
+  status: 'active' | 'inactive';
+  imageUrl: string;
+};
+
+const ContactList = (): JSX.Element => {
+  const contacts: Contact[] = [
     {
       uid: 1,
       name: 'John Doe',
@@ -67,11 +74,51 @@ const ContactList = () => {
 
   return (
     <View>
-      <Text>ContactList</Text>
+      <Text style={styles.headingText}>Contact List</Text>
+      <ScrollView scrollEnabled={false} style={styles.container}>
+        {contacts.map(({uid, name, status, imageUrl}) => (
+          <View key={uid} style={styles.userCard}>
+            <Image
+              source={{
+                uri: imageUrl,
+              }}
+              style={styles.userImage}
+            />
+            <Text style={styles.username}>{name}</Text>
+            <Text style={styles.userStatus}>{status}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
 
 export default ContactList;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+  },
+  container: {},
+  userCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  username: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  userStatus: {
+    color: 'gray',
+  },
+});
