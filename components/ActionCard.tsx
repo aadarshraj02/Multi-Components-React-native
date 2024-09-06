95f511ee5bea9fa31eaf720f6d71ad32d4619094
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-06 17:32:37 +0530

    added typescript return type

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -1,7 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const ActionCard = () => {
+const ActionCard = (): JSX.Element => {
   return (
     <View>
       <Text>ActionCard</Text>
