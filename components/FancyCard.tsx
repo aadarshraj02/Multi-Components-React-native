d77242a4385a4bd0a00f277efb2a0aa83f5080a1
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-06 17:25:31 +0530

    styled cards

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -19,7 +19,7 @@ export default function FancyCard() {
             The biggest centre of Buddhist learning in Spiti Valley, Key
             Monastery is over 1000-year-old. It is the oldest training centre
             for Lamas. It is located at a height of 13,668 feet above mean sea
-            level in Lahaul, Spiti district of Himachal Pradesh in north India.
+            level in Lahaul.
           </Text>
           <Text style={styles.cardFooter}>A dream Place for Travelers!</Text>
         </View>
@@ -29,15 +29,59 @@ export default function FancyCard() {
 }
 
 const styles = StyleSheet.create({
-  headingText: {},
-  card: {},
-  cardElevated: {},
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+  },
+  card: {
+    width: 350,
+    height: 360,
+    borderRadius: 6,
+    marginVertical: 12,
+    marginHorizontal: 16,
+  },
+  cardElevated: {
+    backgroundColor: '#fff',
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
   cardImage: {
     height: 200,
+    marginBottom: 8,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
   },
-  cardBody: {},
-  cardTitle: {},
-  cardLabel: {},
-  cardDescription: {},
-  cardFooter: {},
+  cardBody: {
+    flex: 1,
+    flexGrow: 1,
+    paddingHorizontal: 12,
+  },
+  cardTitle: {
+    color: '#000',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 4,
+    textAlign: 'center',
+  },
+  cardLabel: {
+    color: '#1d1d1d',
+    fontSize: 14,
+    fontWeight: '500',
+    marginBottom: 6,
+  },
+  cardDescription: {
+    color: '#5a5a5a',
+    fontSize: 12,
+    marginBottom: 8,
+    flexShrink: 1,
+  },
+  cardFooter: {
+    color: '#868686',
+    textAlign: 'center',
+  },
 });
