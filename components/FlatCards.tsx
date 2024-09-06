50f8057435acad783698ec625f4dad380a165032
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-06 09:27:07 +0530

    added basic styling for cards and styled cardOne

--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -5,6 +5,11 @@ export default function FlatCards(): JSX.Element {
   return (
     <View>
       <Text style={styles.headingText}>FlatCards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,5 +20,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingHorizontal: 8,
     paddingVertical: 6,
+  },
+  container: {},
+  card: {
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cardOne: {
+    backgroundColor: '#ef5354',
   },
 });
