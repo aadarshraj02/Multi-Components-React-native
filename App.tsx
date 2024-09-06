8ae172c1ab97c3a49e80cecdd25172ee9e35d60b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-06 14:30:02 +0530

    imported FancyCard component

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCards from './components/FlatCards';
 import ElevatedCards from './components/ElevatedCards';
+import FancyCard from './components/FancyCard';
 
 const App = (): JSX.Element => {
   return (
@@ -9,6 +10,7 @@ const App = (): JSX.Element => {
       <ScrollView>
         <FlatCards />
         <ElevatedCards />
+        <FancyCard />
       </ScrollView>
     </SafeAreaView>
   );
