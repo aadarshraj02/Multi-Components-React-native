a596a87c38d343d33a259d3ed4afcd5c08530d64
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-06 09:10:25 +0530

    Imported FLatedCards

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
+import FlatCards from './components/FlatCards';
 
 const App = (): JSX.Element => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text>App</Text>
+        <FlatCards />
       </ScrollView>
     </SafeAreaView>
   );
