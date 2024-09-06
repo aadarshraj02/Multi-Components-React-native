d354cf8e78fe696c585b8c3082eb9ad8a6ff0865
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-06 09:18:19 +0530

    added a heading and Styles Heading

--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -4,9 +4,16 @@ import React from 'react';
 export default function FlatCards(): JSX.Element {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>FlatCards</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+  },
+});
